11696b909e4ba34e3e69895fdbeb58368ca06e11
Chung ye zun <maple0729@kaist.ac.kr>
2019-12-26 16:12:22 +0900

    second commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView a = new TextView(this);
+        a.setText("Hello World");
+        setContentView(a);
+    }
+}
